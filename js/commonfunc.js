82891761a290dbf9b96f0ca6fb0c69c08e8f0018
arzikiii <arzi1605@gmail.com>
2021-06-19 21:49:57 +0900

    button

--- js/commonfunc.js
+++ js/commonfunc.js
@@ -1,91 +1,92 @@
 // Enable button
 function enableButton(index) {
-    controller[index].disabled = false;
+  controller[index].disabled = false;
+  controller[index].style.backgroundColor = "rgb(0,0,0,0)";
 }
 
 // Disable button
 function disableButton(index) {
-    controller[index].disabled = true;
+  controller[index].disabled = true;
+  controller[index].style.backgroundColor = "black";
 }
 
 // play button
 function executePlay() {
-    // Enable button pause
-    enableButton(1);
+  // Enable button pause
+  enableButton(1);
 
-    // Disable button play, nextmove, clear
-    disableButton(0);
-    disableButton(2);
-    disableButton(3);
+  // Disable button play, nextmove, clear
+  disableButton(0);
+  disableButton(2);
+  disableButton(3);
 
-    looper = setInterval(executeNextMove, 200); // do next move every .2 sec
+  looper = setInterval(executeNextMove, 200); // do next move every .2 sec
 }
 
 // pause button
 function executePause() {
-    // Enable button play, next move, clear
-    enableButton(0);
-    enableButton(2);
-    enableButton(3);
+  // Enable button play, next move, clear
+  enableButton(0);
+  enableButton(2);
+  enableButton(3);
 
-    // Disable button pause
-    disableButton(1);
+  // Disable button pause
+  disableButton(1);
 
-    // Clear interval, memberhentikan play
-    clearInterval(looper);
+  // Clear interval, memberhentikan play
+  clearInterval(looper);
 }
 
 // clear tm tape
 function executeClear() {
-    // tmTape to null and tapeCells to null
-    tmTape.innerHTML = "";
-    tapeCells = [];
+  // tmTape to null and tapeCells to null
+  tmTape.innerHTML = "";
+  tapeCells = [];
 
-    // Disable all buttons
-    disableButton(0);
-    disableButton(1);
-    disableButton(2);
-    disableButton(3);
+  // Disable all buttons
+  disableButton(0);
+  disableButton(1);
+  disableButton(2);
+  disableButton(3);
 
-    // Clear interval, memberhentikan play
-    clearInterval(looper);
+  // Clear interval, memberhentikan play
+  clearInterval(looper);
 
-    // Change answer field
-    let ansField = document.getElementById("ans");
-    ansField.textContent = 0;
+  // Change answer field
+  let ansField = document.getElementById("ans");
+  ansField.textContent = 0;
 }
 
-function move(newSymbol, arah, newState, isPushing){
-    if(isPushing){
-        // Tambah blank cell
-        tapeCells.push(new Cell("B"));
-    }
+function move(newSymbol, arah, newState, isPushing) {
+  if (isPushing) {
+    // Tambah blank cell
+    tapeCells.push(new Cell("B"));
+  }
 
-    // Change textContent
-    tapeCells[it].changeTo(newSymbol);
-    tmTape.childNodes[it].textContent = newSymbol;
+  // Change textContent
+  tapeCells[it].changeTo(newSymbol);
+  tmTape.childNodes[it].textContent = newSymbol;
 
-    // Activate cell
-    tmTape.childNodes[it].className += " active";
-    tmTape.childNodes[it + 1].scrollIntoView();
+  // Activate cell
+  tmTape.childNodes[it].className += " active";
+  tmTape.childNodes[it + 1].scrollIntoView();
 
-    // Change state
-    state = newState;
+  // Change state
+  state = newState;
 
-    // Ke Kanan or Ke Kiri or Stay
-    if(arah == 1){
-        it++;
-    }
-    else if(arah == -1){
-        it--;
-    }
+  // Ke Kanan or Ke Kiri or Stay
+  if (arah == 1) {
+    it++;
+  } else if (arah == -1) {
+    it--;
+  }
 }
 
-let i;          // iteration for loop
-let curCell;    // current cell
-let it;         // ++ kekanan, -- kekiri
-let state;      // state
-let looper;     // untuk play
+let i; // iteration for loop
+let curCell; // current cell
+let it; // ++ kekanan, -- kekiri
+let state; // state
+let looper; // untuk play
 let tapeCells = []; // Isi tape turing machine
-const tmTape = document.getElementById("tmTape");// Turing machine tape
-const controller = document.querySelectorAll(".controller");// Button controller
+const tmTape = document.getElementById("tmTape"); // Turing machine tape
+const controller = document.querySelectorAll(".controller"); // Button controller
